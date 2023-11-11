bb53c3332bf3266cc6cf633d8496fcc8a8e5281f
ravichandrapalla <cravi6635@gmail.com>
2023-11-11 19:30:09 +0530

    minor ui fixes

--- dist/normalize.1114379f.js
+++ dist/normalize.1114379f.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64076" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49517" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
